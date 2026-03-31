0fa2478f1f78ad82842f0588b6fea8e1101b4ddc
AbarzuaLuis <luis.abarzua.ing@gmail.com>
2026-03-30 23:58:14 -0300

    Update vite.config.js
    para que cambie la ruta donde está el código dinamico

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  // IMPORTANTE PARA GITHUB PAGES: Cambia esto si tu repositorio no se llama igual
-  base: './', 
+  base: '/hebreo-l/', // <-- ¡Esta línea es la clave para evitar la pantalla blanca!
 })
